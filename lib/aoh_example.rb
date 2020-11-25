7f24b386cdcc4d9810a08ee27bdb0181bdbf220d
Daniel Calderon <danieleduardocald@gmail.com>
2020-11-25 19:06:33 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -8,12 +8,11 @@ BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
 
 def assembled_aoh
   
-  aoh = [
-    { name:  "Don Gately", occupation: "Live-in Staff" },
-    { name:  "Joelle van Dyne", occupation: "Radio Personality" },
-    { name:  "Pat Monteseian", occupation: "Staff" },
-    { name:  "Kate Gompert", occupation: "None" },
-    { name:  "Bruce Green", occupation: "Fan of Mildred" }
+  aoh = [ DON_G = { name:  "Don Gately", occupation: "Live-in Staff" },
+JOELLE_VD = { name:  "Joelle van Dyne", occupation: "Radio Personality" },
+PAT_M =  { name:  "Pat Monteseian", occupation: "Staff" },
+KATE_G = { name:  "Kate Gompert", occupation: "None" },
+BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
     ]
     
   # Build an array that contains (or, "nests") the constants into a single
