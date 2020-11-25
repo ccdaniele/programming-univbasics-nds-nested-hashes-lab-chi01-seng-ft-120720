3e82edfce93aeac5eac16c5d35a084eab12c2c25
Daniel Calderon <danieleduardocald@gmail.com>
2020-11-25 19:09:11 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -8,11 +8,12 @@ BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
 
 def assembled_aoh
   
-  aoh = [ DON_G = { name:  "Don Gately", occupation: "Live-in Staff" },
-JOELLE_VD = { name:  "Joelle van Dyne", occupation: "Radio Personality" },
- PAT_M =  { name:  "Pat Monteseian", occupation: "Staff" },
- KATE_G = { name:  "Kate Gompert", occupation: "None" },
- BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
+  aoh = [
+    { name:  "Don Gately", occupation: "Live-in Staff" },
+    { name:  "Joelle van Dyne", occupation: "Radio Personality" },
+    { name:  "Pat Monteseian", occupation: "Staff" },
+    { name:  "Kate Gompert", occupation: "None" },
+    { name:  "Bruce Green", occupation: "Fan of Mildred" }
     ]
     
   # Build an array that contains (or, "nests") the constants into a single
@@ -44,4 +45,5 @@ def aoh_update(aoh, row, key, new_value)
   
   aoh[row][key] = new_value
   
+
 end
