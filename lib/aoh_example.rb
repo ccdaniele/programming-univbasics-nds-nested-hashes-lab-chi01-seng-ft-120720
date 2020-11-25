2aa357265b9bc6100b191130e0000ec2f4729947
Daniel Calderon <danieleduardocald@gmail.com>
2020-11-25 18:39:38 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -43,7 +43,9 @@ end
 
 def aoh_update(aoh, row, key, new_value)
   
-  aoh[row][key] = new_value
+  aoh = 
+  
+  aoh[row] = {key: new_value}
 
   
   # Update the AoH data at row and key to have the value of new_value
